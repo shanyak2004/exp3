2e5a395f49463bb9442747b98856b1134beed6b7
shanyak2004 <shanyawork30@gmail.com>
2025-04-29 22:45:11 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+        int sum = num1 + num2;  // add the numbers
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
